9ff768418c9593a21c0ccc3403229186c31b1716
joel <joel.uss1728@gmail.com>
2024-08-08 18:02:20 -0700

    devtools autoreload test

--- src/main/java/com/joel/test/proyecto_1/rest/MiController.java
+++ src/main/java/com/joel/test/proyecto_1/rest/MiController.java
@@ -2,13 +2,20 @@ package com.joel.test.proyecto_1.rest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MiController {
 
     @GetMapping("html")
     public String html()
     {
         return "index";
+    }
+
+    @GetMapping("auto")
+    public String devtoolsAutoloadTest()
+    {
+        return "this was saved by devtools reload";
     }
 }
